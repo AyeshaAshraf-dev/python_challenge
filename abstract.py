a21d32a02dfc493a9f6120637f2720eb8734e5f3
Ayesha Ashraf <234213813+AyeshaAshraf-dev@users.noreply.github.com>
2026-06-17 13:08:20 +0500

    abstration and little system design

--- abstract.py
+++ abstract.py
@@ -1,24 +1,76 @@
+# from abc import ABC, abstractmethod
+
+
+# #Abstraction hides unnessasary details
+# #parent class act as a blueprint 
+# class vehicle(ABC):
+#     @abstractmethod
+#     def start(self):
+#         print("hy")
+
+# class Car(vehicle):
+#     def start(self):
+#         print("Car starts with key")
+
+# class Bike(vehicle):
+#     def start(self):
+#         print("bike do hup")
+
+# car = Car()
+# bike = Bike()
+
+# car.start()
+# bike.start()
+    
 from abc import ABC, abstractmethod
 
 
-#Abstraction hides unnessasary details
-#parent class act as a blueprint 
-class vehicle(ABC):
+class Vehicle(ABC):
+
     @abstractmethod
-    def start(self):
-        print("hy")
+    def go(self):
+        pass
 
-class Car(vehicle):
-    def start(self):
-        print("Car starts with key")
+    @abstractmethod
+    def stop(self):
+        pass
 
-class Bike(vehicle):
-    def start(self):
-        print("bike do hup")
 
-car = Car()
-bike = Bike()
+class car(Vehicle):
 
-car.start()
-bike.start()
+    def go(self):
+        print("Car goooooo")
     
+    def stop(self):
+        print("Car stopppp")
+
+
+class motorbike(Vehicle):
+
+    def go(self):
+        print("motobike goooo")
+
+    def stop(self):
+        print("motorbike stawppp")
+
+
+class boat(Vehicle):
+
+    def go(self):
+        print("Boat is goinggg")
+    
+    def stop(self):
+        print("Boat stopped")
+
+
+Car = car()
+Car.go()
+Car.stop()
+
+Motorbike = motorbike()
+Motorbike.go()
+Motorbike.stop()
+
+beet = boat()
+beet.go()
+beet.stop()
